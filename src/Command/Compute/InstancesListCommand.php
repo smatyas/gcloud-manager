85e8c637ccbf7322e4846e5a447e44e547293fbf
Mátyás Somfai <somfai.matyas@gmail.com>
2016-06-04 15:22:07 +0200

    compute:instances:list command added

--- src/Command/Compute/InstancesListCommand.php
+++ src/Command/Compute/InstancesListCommand.php
@@ -13,6 +13,7 @@
 namespace Smatyas\GCloudManager\Command\Compute;
 
 use Knp\Command\Command;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -28,6 +29,30 @@ class InstancesListCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('TODO');
+        // TODO: these should be read from the config json, but they are not set by the Google_Client::setAuthConfig.
+        $project = 'hackathon-1328';
+        $zone = 'europe-west1-d';
+
+        $client = new \Google_Client([]);
+        $client->addScope('https://www.googleapis.com/auth/cloud-platform');
+        $client->setAuthConfig('credentials/hackathon-c87ef1f28578.json');
+
+        $computeService = new \Google_Service_Compute($client);
+        $instances = $computeService->instances->listInstances($project, $zone);
+
+        $table = new Table($output);
+        $table->setHeaders(['ID', 'Name', 'Status']);
+        foreach ($instances as $instance) {
+            if ($instance instanceof \Google_Service_Compute_Instance) {
+                $table->addRow([
+                    $instance->getId(),
+                    $instance->getName(),
+                    $instance->getStatus(),
+                ]);
+            } else {
+                throw new \Exception('Unexpected instance: ' . get_class($instance));
+            }
+        }
+        $table->render();
     }
 }
